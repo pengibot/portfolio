64f7950a14fec19d53f825d2f8c14f7f49576016
pengibot <pengibot@gmail.com>
2023-04-14 17:05:26 +0100

    added code to fetch data from github. Also moved components into seaparate area

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import Header from "./Components/Header";
+import logo from './assets/logo.svg';
 import './App.css';
+import Profile from './pages/Profile';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Header logo={logo}/>
+                <Routes>
+                    <Route
+                        path='/'
+                        element={<Profile userName='pengibot'/>}
+                    />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import './Header.css';
+
+function Header({logo}) {
+    return (
+        <header className='App-header'>
+            <img src={logo} className='App-logo' alt='logo'
+            />
+            <h1>My Portfolio</h1>
+        </header>
+    );
+}
+
+export default Header;
